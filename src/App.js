d0b28d26c71c6453919afbf07301454ce31e02d9
Jeri <jeriacasper@gmail.com>
2022-03-02 16:44:19 -0600

    added some buttons and styling to app.js
    Getting a "shell" started for going forward.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import aclogo from './acnh logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={aclogo} className="ac-logo" alt="ac-logo" />
+    
+        <button className="buttonMain">
+          Add or View Data
+        </button>
+        <button className="buttonMain" >
+          View Stats
+        </button>
       </header>
     </div>
   );
 }
-
 export default App;
