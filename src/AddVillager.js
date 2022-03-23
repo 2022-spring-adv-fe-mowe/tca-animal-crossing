937f147ee8c0d64e313a9305f3fa86ec05aaac08
Jeri <jeriacasper@gmail.com>
2022-03-23 15:25:40 -0500

    Add villager started
    added text field and button for villager's name

--- src/AddVillager.js
+++ src/AddVillager.js
@@ -1,5 +1,24 @@
+import { TextField, Button } from "@mui/material";
+
 export const AddVillager = () => {
 	return(
-		<h2>Add Villager</h2>
+		<>
+			<h2>Add Villager</h2>
+
+			<TextField
+				required
+				id="outlined-required"
+				label="Villager Name"
+				variant="outlined"
+			>
+			</TextField>
+			<Button
+				variant="contained"
+				color="success"
+				size="large"
+			>
+				Submit
+			</Button>
+		</>
 	);
 };
