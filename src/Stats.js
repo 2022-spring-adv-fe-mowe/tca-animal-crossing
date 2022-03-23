1c852ea2bbb5abb31211b68936a295b4a7aafb49
Jeri <jeriacasper@gmail.com>
2022-03-23 17:04:33 -0500

    set up table to display stats

--- src/Stats.js
+++ src/Stats.js
@@ -1,5 +1,24 @@
+import { Table, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+
 export const Stats = () => {
 	return(
+		<>
 		<h2>Stats</h2>
+
+		<TableContainer>
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableCell>Villager</TableCell>
+						<TableCell>Last Interaction Date</TableCell>
+						<TableCell>Last Gift Date</TableCell>
+						<TableCell>Picture?</TableCell>
+						<TableCell>Avg. Weekly Chats</TableCell>
+						<TableCell>Avg. Weekly Gifts</TableCell>
+					</TableRow>
+				</TableHead>
+			</Table>
+		</TableContainer>
+		</>
 	);
 };
