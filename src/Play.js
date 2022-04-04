b4bad1b0cbaf29714a03224cf2e537878579fb9d
Jeri <jeriacasper@gmail.com>
2022-04-04 15:25:09 -0500

    Changed radio select to a drop down menu

--- src/Play.js
+++ src/Play.js
@@ -1,23 +1,83 @@
-import { FormControl, InputLabel, MenuItem, Select, FormGroup, Checkbox, FormControlLabel, RadioGroup, FormLabel, Radio, Button, createTheme } from "@mui/material";
-import { spacing, positions } from "@mui/system";
-import RadioGroupContext from "@mui/material/RadioGroup/RadioGroupContext";
-import { useSelect } from '@mui/base/SelectUnstyled';
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select  from "@mui/material/Select";
+import Button from "@mui/material/Button";
 
 import React from "react";
-
 
 export const Play = ({villagers}) => {
 	const current = new Date();
 	const date = `${current.getMonth()+1}/${current.getDate()}/${current.getFullYear()}`;
+
+	/* const handleChange = (e) => {
+		setGiftExchange(e.target.value);
+	}; */
 	
 	return(
 		<>
 			
-			<h1>Gifting</h1>
+			<h1>Gift Exchange</h1>
 			<h2> {date} </h2>
+
 			{villagers.map(x => 
-				<FormControl>
-					<FormLabel>{x}</FormLabel>
+				<FormControl sx={{ m: 1, minWidth: 120 }}>
+					<InputLabel>{x}</InputLabel>
+					<Select
+						labelId="gift-exchange-label"
+						id="gift-exchange"
+						value={x}
+						label={x}
+						// onChange={handleChange}
+					>
+						<MenuItem value="GIR">
+							Gift with gift in return
+						</MenuItem>
+						<MenuItem value="NIR">
+							Gift with no gift in return						
+						</MenuItem>
+						<MenuItem value="PIR">
+							Gift with villager picture in return
+						</MenuItem>
+						<MenuItem value="na">
+							Unable to gift villager
+						</MenuItem>
+					</Select>
+				</FormControl>				
+			)}
+		<Button
+			variant="contained"
+			color="success"
+			size="large"
+		>
+			Submit
+		</Button>
+		</>
+	);
+};
+
+
+/* <FormGroup>
+					<h3>{x}</h3>
+					<FormControlLabel
+						label="Gift w/ no gift in return"
+						control={<Checkbox />} 
+					/>
+					<FormControlLabel
+						label="Gift w/ gift in return"
+						control={<Checkbox />} 
+					/>
+					<FormControlLabel
+						label="Gift w/ picture in return"
+						control={<Checkbox />} 
+					/>
+						<FormControlLabel
+						label="Unable to gift villager"
+						control={<Checkbox />} 
+					/>
+				</FormGroup> */
+
+/* <FormLabel>{x}</FormLabel>
 					<RadioGroup>
 						<FormControlLabel 
 							value="GIR"
@@ -39,37 +99,4 @@ export const Play = ({villagers}) => {
 						control={<Radio />}
 						label="Unable to gift villager"
 						/>
-					</RadioGroup>
-				</FormControl>				
-			)}
-		<Button
-			variant="contained"
-			color="success"
-			size="large"
-		>
-			Submit
-		</Button>
-		</>
-	);
-};
-
-
-{/* <FormGroup>
-					<h3>{x}</h3>
-					<FormControlLabel
-						label="Gift w/ no gift in return"
-						control={<Checkbox />} 
-					/>
-					<FormControlLabel
-						label="Gift w/ gift in return"
-						control={<Checkbox />} 
-					/>
-					<FormControlLabel
-						label="Gift w/ picture in return"
-						control={<Checkbox />} 
-					/>
-						<FormControlLabel
-						label="Unable to gift villager"
-						control={<Checkbox />} 
-					/>
-				</FormGroup> */}
+					</RadioGroup> */
