d7e5498e66f2727363ad71e94035e09e42bbbef4
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2022-04-24 20:23:26 -0500

    Complex reduce()  to calculate most recent gift exchange from all gift exchanges...

--- src/Stats.js
+++ src/Stats.js
@@ -4,12 +4,49 @@ import { useNavigate } from "react-router-dom";
 export const Stats = ({giftExchanges}) => {
 
 	const nav = useNavigate();
-
+	
 	// Let's see if new data makes it here ? ? ?
 	//
 	// From 'lifted' state in App.js.
 	//
 	console.log(giftExchanges);
+
+	// Since it does, let's shape the data how we want it for display... reduce FTW ! ! !
+	const mostRecentGiftExchanges = [
+
+		// Group by name and only save the most recent gift exchange.
+		...giftExchanges.reduce(
+			(acc, x) => {
+				const currentGiftExchangeForVillagerInMap = acc.get(x.name);
+
+				acc.set(
+					x.name
+					, 
+						currentGiftExchangeForVillagerInMap
+
+						// If it already exists, update it only if a more
+						// recent gift exchange
+						? {
+							name: x.name
+							, date: x.date > currentGiftExchangeForVillagerInMap.date ? x.date : currentGiftExchangeForVillagerInMap.date
+							, giftExchange: x.date > currentGiftExchangeForVillagerInMap.date ? x.giftExchange : currentGiftExchangeForVillagerInMap.giftExchange
+						}
+
+						// If doesn't exist, add it.
+						: {
+							name: x.name
+							, date: x.date
+							, giftExchange: x.giftExchange
+						}
+				)
+
+				return acc;
+			}
+			, new Map()
+		)
+	].map(x => x[1]); // Spreading a Map into an array yields an array with two items, the object we want is the second item... Tricky Woo for sure ! ! !
+
+	console.log("mostRecentGiftExchanges", mostRecentGiftExchanges);
 
 	return(
 		<>
@@ -26,7 +63,7 @@ export const Stats = ({giftExchanges}) => {
 						</TableRow>
 					</TableHead>
 					<TableBody>
-						{giftExchanges.map(giftExchange => 
+						{mostRecentGiftExchanges.map(giftExchange => 
 						<TableRow 
 									key={giftExchange.name}
 							>
