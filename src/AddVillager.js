9aa9a40acb170caad7fb2263252aca2518615127
Jeri <jeriacasper@gmail.com>
2022-04-30 16:10:44 -0500

    starting on Add Villager component

--- src/AddVillager.js
+++ src/AddVillager.js
@@ -1,7 +1,33 @@
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 
+import { useState } from "react";
+
 export const AddVillager = () => {
+
+	const currentDate = new Date();
+	const date = `${currentDate.getMonth()+1}/${currentDate.getDate()}/${currentDate.getFullYear()}`; 
+
+	const [newvillager, setNewVillager] = useState(villagers.map(x => ({
+		name: x.villagerName,
+		giftExchange: "Villager added to island",
+		date: date,
+		picture: false,
+		active: true
+	})));
+
+	const handleChange = (villager, newVillager) => {
+		setNewVillager(
+			newVillager.map(x => ({
+				...x, 
+
+			}))
+		)
+	}
+	
+	const villager = {
+		
+	}
 	
 	return(
 		<>
@@ -12,12 +38,15 @@ export const AddVillager = () => {
 				id="outlined-required"
 				label="Villager Name"
 				variant="outlined"
+				value={villagerName}
+				onChange={(e) => handleChange(x, e.target.value)}
 			>
 			</TextField>
 			<Button
 				variant="contained"
 				color="success"
 				size="large"
+				onClick={addNewVillager}
 			>
 				Submit
 			</Button>
